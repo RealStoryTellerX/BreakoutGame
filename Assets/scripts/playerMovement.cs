1fb3154e1b025f264e76d5d6677cbf0aa6f02f3e
Shekhar Sai <saishekhar7@gmail.com>
2023-02-07 02:20:10 -0500

    -setup

---
+++ Assets/scripts/playerMovement.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    // Update is called once per frame
+
+
+    /*
+     * TODO       
+     * implement WASD/ mouse movement
+     * 
+     */
+
+    /*
+     * TODO       
+     * implement screen-bounds restriction movement
+     * 
+     */
+
+    void FixedUpdate()
+    {
+        
+    }
+}
